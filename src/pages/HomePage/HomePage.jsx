fb210ae4ecf62c691d4a37021b3ab6e485ba4ccc
MWWtz <martinwiytiszen@gmail.com>
2023-03-02 15:53:14 -0300

    overflow fixed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import TopNavbar from "../../components/TopNavbar";
 import Title from "../../components/Title";
 import Dropdown from "../../components/Dropdown";
@@ -8,14 +8,19 @@ import styles from "../../styles/HomePage.module.scss";
 
 export default function HomePage() {
   const { playingNow } = useContext(AppContext);
-  const { title, original_title, poster_path } = playingNow;
+  const { title, original_title, poster_path, backdrop_path } = playingNow;
 
   return (
     <div className={styles.container}>
       <TopNavbar />
       <div className={styles.main}>
         <img
-          className={styles.playingNow}
+          className={styles.playingNowDesktop}
+          src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+          alt="bg"
+        />
+        <img
+          className={styles.playingNowMobile}
           src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
           alt="bg"
         />
